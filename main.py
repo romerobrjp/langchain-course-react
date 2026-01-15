717fff0c0ca122d2cc763d30dfdad9938a67a780
Romero Medeiros <romerobrjp@gmail.com>
2026-01-15 11:09:11 -0300

    refactor(main): enhance main function and add tool lookup functionality

--- main.py
+++ main.py
@@ -2,25 +2,34 @@ from langchain.tools import tool
 from langchain_core.tools import render_text_description
 from langchain_core.prompts import PromptTemplate
 from langchain_classic.agents.output_parsers.react_single_input import ReActSingleInputOutputParser
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain_core.tools import BaseTool
+from typing import Sequence, Union
 from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
 load_dotenv()
 
 @tool
-def get_length_of_string(s: str) -> int:
+def get_length_of_string(s: str)->int:
     """Returns the length of the input string."""
-    print(f"get_text_length enter with {s=}")
+    print(f">> get_text_length enter with {s=}")
     s = s.strip("'\n").strip('"')
     return len(s)
 
-def main(str):
-    print("Hello from langchain-course-react!")
-    print(f"Its length is: {get_length_of_string.invoke(str)}")
+def find_tool_by_name(tools: Sequence[BaseTool], name: str) -> BaseTool:
+    for tool in tools:
+        if tool.name == name:
+            return tool
+    raise ValueError(f"Tool with name {name} not found")
+
+def main(input_str: str):
+    print(">> Hello from langchain-course-react!")
+    print(f">> Its length is: {get_length_of_string.invoke(input_str)}")
 
 if __name__ == "__main__":
     main('lion')
     tools = [get_length_of_string]
-    print(f"Available tools: {[tool.name for tool in tools]}")
+    print(f">> Available tools: {[tool.name for tool in tools]}")
     template = """
     Answer the following questions as best as you can. You have access to the following tools:
 
@@ -48,5 +57,14 @@ if __name__ == "__main__":
     llm = ChatOllama(model="qwen2.5", temperature=0, stop=["\nObservation", "Observation", "Observation:"])
     agent = { "input": lambda x: x["input"] } | prompt | llm | ReActSingleInputOutputParser()
 
-    response = agent.invoke({"input": "What is the length of the word 'lion'?"})
-    print(response)
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input": "What is the length of the word 'lion'?"})
+    print(agent_step)
+
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+        tool_output = tool_to_use.invoke(tool_input)
+        print(f">> Tool {tool_name} output: {tool_output}")
+    elif isinstance(agent_step, AgentFinish):
+        print(f">> Final answer: {agent_step.return_values}")
